3086492c2ee956159ea3c8d9331df7498a108e1f
FATx64 <191420072@student.binadarma.ac.id>
2022-07-16 10:00:36 +0700

    finishing calc

--- Kalkulator/assets/calc.js
+++ Kalkulator/assets/calc.js
@@ -1,21 +1,97 @@
-const firstName = prompt("Siapa nama depanmu?");
-const lastName = prompt("Siapa nama belakangmu?");
-const language = prompt("Bisa berbahasa apa?");
+//menyimpan data awal
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+  };
+
+  //untuk mengupdate angka di layar
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+  }
+   
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+  }
+
+  function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+  }
+
+const buttons = document.querySelectorAll('.button');
  
-const user = {
-   name: {
-       first: firstName,
-       last: lastName,
-   },
-   language: language
-};
+for (const button of buttons) {
+  button.addEventListener('click', function (event) {
+    // mendapatkan objek elemen yang diklik
+    const target = event.target;
  
-if (user.language === "English") {
-   alert("Nice to meet you " + user.name.first + " " + user.name.last + "!");
-} else if (user.language === "French") {
-   alert("Ravi de vous rencontrer " + user.name.first + " " + user.name.last + "!");
-} else if (user.language === "Japanese") {
-   alert("Hajimemashite, " + user.name.first + " " + user.name.last + "!");
-} else {
-   alert("Senang bertemu dengan Anda " + user.name.first + " " + user.name.last + "!");
+    if (target.classList.contains('clear')) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+ 
+    if (target.classList.contains('negative')) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains('equals')) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+    if (target.classList.contains('operator')) {
+      handleOperator(target.innerText);
+      return;
+    }
+ 
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
 }
+
+//fungsi untuk tombol -/+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+      return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+  }
+  function handleOperator(operator) {
+    if (!calculator.isWaitForSecondNumber) {
+      calculator.operator = operator;
+      calculator.isWaitForSecondNumber = true;
+      calculator.firstNumber = calculator.displayNumber;
+   
+      // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+      calculator.displayNumber = '0';
+    } else {
+      alert('Operator sudah ditetapkan');
+    }
+}
+
+//fungsi hitung tambah dan kurang
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+      alert('Anda belum menetapkan operator');
+      return;
+    }
+   
+    let result = 0;
+    if (calculator.operator === '+') {
+      result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+      result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+   
+    calculator.displayNumber = result;
+  }
