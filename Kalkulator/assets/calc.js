79cb6af4988461e6f25dca63e3c67c99b63035d2
FATx64 <191420072@student.binadarma.ac.id>
2022-07-16 13:37:34 +0700

    up to sub 1

--- Kalkulator/assets/calc.js
+++ Kalkulator/assets/calc.js
@@ -92,6 +92,15 @@ function performCalculation() {
     } else {
       result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
+
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
    
+    putHistory(history)
     calculator.displayNumber = result;
+    renderHistory();
   }
